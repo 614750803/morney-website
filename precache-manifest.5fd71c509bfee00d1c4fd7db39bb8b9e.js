468a8034345ba57fb28dd4d419239f4d2742d67a
Songsong <614750803@qq.com>
2020-09-22 15:00:26 +0800

    deploy

---
+++ precache-manifest.5fd71c509bfee00d1c4fd7db39bb8b9e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "901a59f30be7129903af",
+    "url": "/morney-website/css/app.a9b76896.css"
+  },
+  {
+    "revision": "621124190f3df628029d90f5261f588c",
+    "url": "/morney-website/index.html"
+  },
+  {
+    "revision": "901a59f30be7129903af",
+    "url": "/morney-website/js/app.470d3abb.js"
+  },
+  {
+    "revision": "18e58caa671679b2e2e1",
+    "url": "/morney-website/js/chunk-vendors.c804dfd5.js"
+  },
+  {
+    "revision": "34fa58f264e125e51ff260d77309bdbf",
+    "url": "/morney-website/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/morney-website/robots.txt"
+  }
+]);
